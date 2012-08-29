859aabf74f7024ad5ac6b483b30125777a50b81d
L Serj <gelvior@gmail.com>
2012-08-29 20:13:41 +0400

    Tests added to test Setting's link.

--- spec/requests/links_spec.rb
+++ spec/requests/links_spec.rb
@@ -5,18 +5,36 @@ describe "Links" do
   before(:each){ @user = FactoryGirl.create(:user) }
 
   describe "of footer" do
-    it "should show link to login for non-signed users" do
-      visit '/'
-      response.should have_selector("a", :href => new_user_session_path, :content => "Войти" )
+    describe "for non-signed users" do
+      it "should show link to login" do
+        visit '/'
+        response.should have_selector("a", :href => new_user_session_path, :content => "Войти" )
+      end
+
+      it "should not show setting's link" do
+        visit '/'
+        response.should_not have_selector("a", :href => edit_user_registration_path, :content => "Настройки" )
+      end
     end
 
-    it "should not show link to login for signed-in user" do
-      visit '/'
-      click_link "Войти"
-      fill_in "Ваш email",  :with => @user.email
-      fill_in "Пароль", :with => 'qwerty'
-      click_button "Войти"
-      response.should_not have_selector("a", :href => new_user_session_path, :content => "Войти" )
+    describe "for signed-in users" do
+      before(:each) do
+        visit '/'
+        click_link "Войти"
+        fill_in "Ваш email",  :with => @user.email
+        fill_in "Пароль", :with => 'qwerty'
+        click_button "Войти"
+      end
+
+      it "should not show link to login" do
+        visit '/'
+        response.should_not have_selector("a", :href => new_user_session_path, :content => "Войти" )
+      end
+
+      it "should show setting's link" do
+        visit '/'
+        response.should have_selector("a", :href => edit_user_registration_path, :content => "Настройки" )
+      end
     end
   end
 end
