c8a658c7166e522bbc9cf11137071b700dd15d00
L Serj <gelvior@gmail.com>
2012-08-31 18:46:46 +0400

    Helper added to highlight current link on the page.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  # Use this to highlight active page's (toolbar or link).
+  def active_link?( path )
+    if current_page?( path )
+      "active"
+    else
+      ""
+    end
+  end
 end
