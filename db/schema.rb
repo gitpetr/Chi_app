dbe89d970e1192259c38408603258aa213865e7d
L Serj <gelvior@gmail.com>
2012-09-04 20:26:54 +0400

    Article model created.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120825100656) do
+ActiveRecord::Schema.define(:version => 20120904161859) do
+
+  create_table "articles", :force => true do |t|
+    t.text     "content"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "users", :force => true do |t|
     t.string   "email",                  :default => "",    :null => false
