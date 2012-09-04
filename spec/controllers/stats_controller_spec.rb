8fb5c636cece95ed1f1531474a45e6499bb314f9
L Serj <gelvior@gmail.com>
2012-09-04 18:04:45 +0400

    Test created to test user's stats (controller).

--- spec/controllers/stats_controller_spec.rb
+++ spec/controllers/stats_controller_spec.rb
@@ -1,5 +1,25 @@
 require 'spec_helper'
 
 describe StatsController do
+  render_views
 
+  before(:each){ @admin = FactoryGirl.create( :admin ) }
+
+  describe "GET 'users_in_system'" do
+    describe "for non-signed users" do
+      it "should deny access" do
+        get :users_in_system, :locale => :en
+        response.should redirect_to( root_path )
+      end
+    end
+
+    describe "for signed-in admin" do
+      before(:each){ test_sign_in( @admin ) }
+
+      it "should access" do
+        get :users_in_system, :locale => :en
+        response.should be_success
+      end
+    end
+  end
 end
