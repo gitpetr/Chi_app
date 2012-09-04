877f48fdf90f14f3ffbe55ae5b400eb9260c823e
L Serj <gelvior@gmail.com>
2012-09-04 18:05:16 +0400

    Code added to pass tests.

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -1,4 +1,8 @@
 class StatsController < ApplicationController
+  before_filter do
+    redirect_to :root unless current_user && current_user.admin?
+  end
+
   def users_in_system
     @users = User.all
   end
