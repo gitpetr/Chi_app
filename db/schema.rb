aa5bb35946b7822b1360b428fabf9c49b4d3732a
L Serj <gelvior@gmail.com>
2012-09-04 20:37:03 +0400

    Migration changed. Now article have id of user.

--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,7 @@
 ActiveRecord::Schema.define(:version => 20120904161859) do
 
   create_table "articles", :force => true do |t|
+    t.integer  "user_id"
     t.text     "content"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
