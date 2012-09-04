667a5788461824a76c6801eafb2c6935b0ef4044
L Serj <gelvior@gmail.com>
2012-09-04 22:00:46 +0400

    Simple article creating realized.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,20 @@
 class ArticlesController < ApplicationController
-  def new
+  def index
 
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new( params[:article] )
+
+    if @article.save
+      redirect_to articles_path
+      flash[:success] = "It has been created!"
+    else
+      render 'new'                                                                        # It should be placed after flash message or you will have to click button twice.
+    end
   end
 end
