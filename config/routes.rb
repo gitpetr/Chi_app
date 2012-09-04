0ccd6c09f7410db547d9ba7b31c4ff46d30c84ab
L Serj <gelvior@gmail.com>
2012-09-04 21:39:48 +0400

    Article controller initialized with empty specs, and new action and other stuff.

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ ChirchApp::Application.routes.draw do
       get '/users/sign_out' => 'devise/sessions#destroy'                                  # Unfortunately i use that because Webrate generated GET method for logout, not DELETE.
     end
 
+    resources :articles, :only => [ :new ]
+
     match '/users_list', :to => 'stats#users_in_system'
     match '/about', :to => 'pages#about'
 
