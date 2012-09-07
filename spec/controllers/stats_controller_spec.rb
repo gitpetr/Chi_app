0d99e17f9c3a8f7acc67c381bf6fc69677b02455
L Serj <gelvior@gmail.com>
2012-09-07 16:00:00 +0400

    Test added for stats controller.

--- spec/controllers/stats_controller_spec.rb
+++ spec/controllers/stats_controller_spec.rb
@@ -3,10 +3,22 @@ require 'spec_helper'
 describe StatsController do
   render_views
 
-  before(:each){ @admin = FactoryGirl.create( :admin ) }
+  before(:each) do
+    @admin = FactoryGirl.create( :admin )
+    @user  = FactoryGirl.create( :user )
+  end
 
   describe "GET 'users_in_system'" do
     describe "for non-signed users" do
+      it "should deny access" do
+        get :users_in_system, :locale => :en
+        response.should redirect_to( root_path )
+      end
+    end
+
+    describe "for signed-in users" do
+      before(:each){ test_sign_in( @user ) }
+
       it "should deny access" do
         get :users_in_system, :locale => :en
         response.should redirect_to( root_path )
