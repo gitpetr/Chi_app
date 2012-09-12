d211d2858a8bc6bf95c344636f181ee47b4ad8d6
L Serj <gelvior@gmail.com>
2012-09-12 23:22:28 +0400

    Now everybody can edit article.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,3 +1,4 @@
+# encoding: UTF-8
 class ArticlesController < ApplicationController
   load_and_authorize_resource
 
@@ -17,6 +18,18 @@ class ArticlesController < ApplicationController
       flash[:success] = t(:article_created_message)
     else
       render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @article.update_attributes( params[:article] )
+      redirect_to articles_path
+      flash[:success] = "Статья успешно обновлена (tranlste it!)!"
+    else
+      render 'edit'
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ ChirchApp::Application.routes.draw do
       get '/users/sign_out' => 'devise/sessions#destroy'                                  # Unfortunately i use that because Webrate generated GET method for logout, not DELETE.
     end
 
-    resources :articles, :only => [ :index, :show, :new, :create ]
+    resources :articles, :only => [ :index, :show, :new, :create, :edit, :update ]
 
     match '/users_list', :to => 'stats#users_in_system'
 
