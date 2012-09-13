6486d0d11a11cf682d6eb4d7219dec3a3f07dbd1
L Serj <gelvior@gmail.com>
2012-09-13 21:40:04 +0400

    Tests added for controller (post put still doesn't work) and integration tests for button and article's updating.

--- spec/controllers/articles_controller_spec.rb
+++ spec/controllers/articles_controller_spec.rb
@@ -91,44 +91,96 @@ describe ArticlesController do
     end
   end
 
-  # describe "POST 'create'" do
-  #   before(:each){ @article = FactoryGirl.build(:article, :user => @admin) }
+  describe "POST 'create'" do
+    before(:each) do
+      @article = FactoryGirl.build(:article, :user => @admin, :content => "some", :title => "bla-bla")
+    end
 
-  #   describe "for non-signed users" do
-  #     it "should deny access" do
-  #       post :create, :locale => :en, :article => @article.attributes.merge( :content => "some" )
-  #       response.should redirect_to( root_path )
-  #     end
+    describe "for non-signed users" do
+      it "should deny access" do
+        post :create, :locale => :en, :article => @article
+        response.should redirect_to( root_path )
+      end
 
-  #     it "should not create article" do
-  #       expect do
-  #         post :create, :locale => :en, :article => @article.attributes.merge(:content => "some" )
-  #       end.should_not change( Article, :count )
-  #     end
-  #   end
+      it "should not create article" do
+        expect do
+          post :create, :locale => :en, :article => @article
+        end.should_not change( Article, :count )
+      end
+    end
 
-  #   describe "for signed-in users" do
-  #     before(:each){ test_sign_in( @user ) }
+    describe "for signed-in users" do
+      before(:each){ test_sign_in( @user ) }
 
-  #     it "should deny access" do
-  #       post :create, :locale => :en, :article => @article.attributes.merge( :content => "some" )
-  #       response.should redirect_to( root_path )
-  #     end
+      it "should deny access" do
+        post :create, :locale => :en, :article => @article
+        response.should redirect_to( root_path )
+      end
 
-  #     it "should not create article" do
-  #       expect do
-  #         post :create, :locale => :en, :article => @article.attributes.merge(:content => "some" )
-  #       end.should_not change( Article, :count )
-  #     end
-  #   end
+      it "should not create article" do
+        expect do
+          post :create, :locale => :en, :article => @article
+        end.should_not change( Article, :count )
+      end
+    end
 
     # describe "for signed-in admin" do
-    #   before(:each){ test_sign_in( @admin ) }
     #   it "should create article" do
+    #     sign_in :article, @admin
+
     #     expect do
-    #       post :create, :locale => :en, :article => @article.attributes.merge(:content => "some" )
+    #       post :create, :locale => :en, :article => @article#.attributes.merge(:content => "some" )
+    #       # post :create, :locale => :en, :articles => { :content => "some", :title => "text" }, :user_id => @admin.id
     #     end.should change( Article, :count ).by(1)
     #   end
     # end
+  end
+
+  describe "GET 'edit'" do
+    before(:each) do
+      @article = FactoryGirl.create(:article, :user => @admin, :content => "some", :title => "bla-bla")
+    end
+
+    describe "for non-signed users" do
+      it "should deny access" do
+        get :edit, :locale => :en, :id => @article
+        response.should redirect_to( root_path )
+      end
+    end
+
+    describe "for signed-in users" do
+      before(:each){ test_sign_in( @user ) }
+
+      it "should deny access" do
+        get :edit, :locale => :en, :id => @article
+        response.should redirect_to( root_path )
+      end
+    end
+
+    describe "for signed-in admin" do
+      before(:each){ test_sign_in( @admin ) }
+
+      it "should access" do
+        get :edit, :locale => :en, :id => @article
+        response.should be_success
+      end
+    end
+  end
+
+  # describe "PUT 'update'" do
+  #   before(:each) do
+  #     @article = FactoryGirl.create(:article, :user => @admin, :content => "some", :title => "bla-bla")
+  #   end
+
+  #   describe "for signed-in admin" do
+  #     before(:each){ test_sign_in( @admin ) }
+
+  #     it "should be done" do
+  #       text = "last"
+  #       put :update, :locale => :en, :id => @article, :article => @article.attributes.merge( :title => text )
+  #       @article.reload
+  #       @article.title.should == text
+  #     end
+  #   end
   # end
 end
--- spec/requests/admins_spec.rb
+++ spec/requests/admins_spec.rb
@@ -22,19 +22,47 @@ describe "Admins" do
   end
 
   describe "Article" do
-    it "should have button to create new article" do
-      click_link "Статьи"
-      response.should have_selector( 'a', :content => 'Создать новую статью' )
+    describe "Buttons" do
+      it "should have button to create new article" do
+        click_link "Статьи"
+        response.should have_selector( 'a', :content => 'Создать новую статью' )
+      end
+
+      it "should have button to edit article for existing articles" do
+        @article = FactoryGirl.create( :article )
+
+        click_link "Статьи"
+        response.should have_selector( 'h2' ) do |h2|
+          h2.should have_selector( 'a', :content => 'Редактировать' )
+        end
+      end
     end
 
-    it "should be created" do
-      expect do
+    describe "Creation" do
+      it "should be done with valid attrs" do
+        expect do
+          click_link "Статьи"
+          click_link "Создать новую статью"
+          fill_in "Заголовок",  :with => "random text"
+          fill_in "Текст статьи",  :with => "random text"
+          click_button "Создать"
+        end.should change( Article, :count ).by( 1 )
+      end
+    end
+
+    describe "Updating" do
+      it "should be done with valid attrs" do
+        @article = FactoryGirl.create( :article ); title = "title text"; content = "content text"
+
         click_link "Статьи"
-        click_link "Создать новую статью"
-        fill_in "Заголовок",  :with => "random text"
-        fill_in "Текст статьи",  :with => "random text"
-        click_button "Создать"
-      end.should change( Article, :count ).by( 1 )
+        click_link "Редактировать"
+        fill_in "Заголовок",  :with => title
+        fill_in "Текст статьи",  :with => content
+        click_button "Обновить"
+
+        response.should have_selector( 'h2' ){ |h2| h2.should have_selector( 'a', :content => title ) }
+        response.should have_selector( 'p', :content => content )
+      end
     end
   end
 end
