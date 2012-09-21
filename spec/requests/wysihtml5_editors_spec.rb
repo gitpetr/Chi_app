9cff2d249e5ecd5bcc0000288d32b7b6ec825efa
L Serj <gelvior@gmail.com>
2012-09-21 22:08:50 +0400

    Test added for editor's toolbar existance (new article).

---
+++ spec/requests/wysihtml5_editors_spec.rb
@@ -0,0 +1,25 @@
+# encoding: UTF-8
+require 'spec_helper'
+
+describe "Wysihtml5Editors" do
+  before(:each) do
+    @admin = FactoryGirl.create(:admin)
+
+    visit '/'
+    click_link "Войти"
+    fill_in "Ваш email",  :with => @admin.email
+    fill_in "Пароль", :with => 'qwerty'
+    click_button "Войти"
+  end
+
+  describe "Panel" do
+    before(:each) do
+      click_link "Статьи"
+      click_link "Создать новую статью"
+    end
+
+    it "should have toolbar", :js => true do
+      page.should have_selector( 'ul', :class => 'wysihtml5-toolbar' )
+    end
+  end
+end
