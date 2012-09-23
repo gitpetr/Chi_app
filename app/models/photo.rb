50c2c922ee7b9a0ca7701580f4f5cd751f7d7d1a
L Serj <gelvior@gmail.com>
2012-09-23 12:56:58 +0400

    Validations with test and translation added for photo model.

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,3 +1,5 @@
 class Photo < ActiveRecord::Base
   attr_accessible :description
+
+  validates :description, :length => { :maximum => 500, :message => :max_lenght_message }
 end
--- spec/models/photo_spec.rb
+++ spec/models/photo_spec.rb
@@ -1,5 +1,23 @@
 require 'spec_helper'
 
 describe Photo do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each){ @attr = { :description => "some text is here" } }
+
+  describe "DB" do
+    it "should create with valid params" do
+      expect do
+        Photo.create( @attr )
+      end.should change( Photo, :count ).by( 1 )
+    end
+  end
+
+  describe "Validations for" do
+    it "description can be empty" do
+      Photo.new( @attr.merge( :description => "" ) ).should be_valid
+    end
+
+    it "description can't contain more than 500 symbols" do
+      Photo.new( @attr.merge( :description => "a" * 501 ) ).should_not be_valid
+    end
+  end
 end
