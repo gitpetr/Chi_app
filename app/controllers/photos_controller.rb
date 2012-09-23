3de657d2de39e9d0e3bac51930e76c35e3231887
L Serj <gelvior@gmail.com>
2012-09-24 00:09:29 +0400

    Security improvements added for photo model.

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,15 +1,13 @@
 class PhotosController < ApplicationController
-  def index
+  load_and_authorize_resource
 
+  def index
   end
 
   def new
-    @photo = Photo.new
   end
 
   def create
-    @photo = Photo.new( params[:photo] )
-
     if @photo.save
       redirect_to photos_path
       flash[:success] = t( :photo_created_message )
--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,11 +4,13 @@ class Ability
   def initialize(user)
     # Define abilities for the passed in user here:
     user ||= User.new # guest user (not logged in)
+
     if user.admin?
       can :manage, :all
     else
       can :index, :home
       can :read, Article
+      can :read, Photo
       # can :read, :all
     end
     #
