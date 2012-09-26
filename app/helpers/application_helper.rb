b9e06216e50636562d5b26e2daf86b4918d457c4
L Serj <gelvior@gmail.com>
2012-09-26 20:50:53 +0400

    Highlight link for root path.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,11 @@
 module ApplicationHelper
 
   # Use this to highlight active page's (toolbar or link).
-  def active_link?( path )
+  # root_path - we pass this path only if we want highlight link for 2 different paths.
+  def active_link?( path, root_path=nil )
     if current_page?( path )
+      "active"
+    elsif !root_path.nil? and current_page?( root_path )
       "active"
     else
       ""
