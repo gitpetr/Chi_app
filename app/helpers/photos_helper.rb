9b398256b54ff363fcdb236cc38d10cd6edd9aa4
L Serj <gelvior@gmail.com>
2012-09-26 19:04:01 +0400

    Show photos inline.

--- app/helpers/photos_helper.rb
+++ app/helpers/photos_helper.rb
@@ -1,2 +1,8 @@
 module PhotosHelper
+  # Helper to show photo inline.
+  def photos_inline( photo )
+    content_tag(:div, :class => 'span4') do
+      image_tag photo.image_url(:thumb), :style => "padding: 10px;" unless photo.nil?
+    end
+  end
 end
