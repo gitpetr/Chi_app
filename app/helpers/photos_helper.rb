2e40df2f4a6e077d221ee422838bddcc3fd9a90b
L Serj <gelvior@gmail.com>
2012-09-27 15:41:59 +0400

    Using partial instead of helper for show photo item.

--- app/helpers/photos_helper.rb
+++ app/helpers/photos_helper.rb
@@ -1,8 +1,2 @@
 module PhotosHelper
-  # Helper to show photo inline.
-  def photos_inline( photo )
-    content_tag(:div, :class => 'span4') do
-      image_tag photo.image_url(:thumb), :style => "padding: 10px;" unless photo.nil?
-    end
-  end
 end
