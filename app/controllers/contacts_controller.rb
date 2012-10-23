f0e2360703da16b5c28f719ea2d7cafa2545404e
L Serj <gelvior@gmail.com>
2012-10-23 20:20:37 +0400

    Now admin can edit and update contacts.

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,4 +1,6 @@
 class ContactsController < ApplicationController
+  load_and_authorize_resource
+
   def index
     @contact = Contact.first.nil? ? nil : Contact.first
   end
@@ -7,5 +9,11 @@ class ContactsController < ApplicationController
   end
 
   def update
+    if @contact.update_attributes( params[:contact] )
+      redirect_to contacts_path
+      flash[:success] = "Contacts updated!"
+    else
+      render 'edit'
+    end
   end
 end
