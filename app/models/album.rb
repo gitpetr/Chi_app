661effda9bafe369444976eb56528707f29c0aa8
L Serj <gelvior@gmail.com>
2013-04-16 22:17:13 +0400

    Album annotated.

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: albums
+#
+#  id          :integer          not null, primary key
+#  name        :string(255)
+#  description :text
+#  user_id     :integer
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 class Album < ActiveRecord::Base
   attr_accessible :description, :name, :user_id
 end
