3a270068de688b9852c6a8c9694f9ce4c88e645c
L Serj <gelvior@gmail.com>
2013-04-16 22:11:08 +0400

    Album's model created.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121026194219) do
+ActiveRecord::Schema.define(:version => 20130416180604) do
+
+  create_table "albums", :force => true do |t|
+    t.string   "name"
+    t.text     "description"
+    t.integer  "user_id"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+  end
+
+  add_index "albums", ["user_id"], :name => "index_albums_on_user_id"
 
   create_table "articles", :force => true do |t|
     t.integer  "user_id"
