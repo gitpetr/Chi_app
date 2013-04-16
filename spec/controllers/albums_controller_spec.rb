46e0e7d307bbb41199f63ece272e7b6d69a9dc57
L Serj <gelvior@gmail.com>
2013-04-16 22:56:03 +0400

    Tests for index method.

--- spec/controllers/albums_controller_spec.rb
+++ spec/controllers/albums_controller_spec.rb
@@ -1,5 +1,37 @@
 require 'spec_helper'
 
 describe AlbumsController do
+  render_views
 
+  before(:each) do
+    @admin = FactoryGirl.create( :admin )
+    @user  = FactoryGirl.create( :user )
+  end
+
+  describe "GET 'index'" do
+    describe "for non-signed users" do
+      it "should access" do
+        get :index, :locale => :en
+        response.should be_success
+      end
+    end
+
+    describe "for signed-in users" do
+      before(:each){ test_sign_in( @user ) }
+
+      it "should access" do
+        get :index, :locale => :en
+        response.should be_success
+      end
+    end
+
+    describe "for signed-in admin" do
+      before(:each){ test_sign_in( @admin ) }
+
+      it "should access" do
+        get :index, :locale => :en
+        response.should be_success
+      end
+    end
+  end
 end
