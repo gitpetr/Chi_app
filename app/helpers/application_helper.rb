17af17d7140845cc1f81b1c110db2f9bf26a76a0
L Serj <gelvior@gmail.com>
2013-04-22 23:36:51 +0400

    Correct links highlighting for nested models.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,11 @@
 module ApplicationHelper
 
   # Use this to highlight active page's (toolbar or link).
-  # name - name of controller with whom link will assosiated.
+  # names - list of names for controllers with whom links will assosiated.
   # current_controller - name of current controller.
-  def active_link?( name, current_controller )
-    if name == current_controller
-      "active"
-    else
-      ""
-    end
+  def active_link?( names, current_controller )
+    names.each{ |name| return "active" if name == current_controller }
+
+    return ""
   end
 end
