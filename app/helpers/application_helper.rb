d9ea0540edb50777ba2a6b96996dcab0751dae21
L Serj <gelvior@gmail.com>
2013-05-12 19:53:42 +0400

    Helper (to highlight links in navbar) has been refactored. Now it is supporting method of a controller to assosiate.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,27 @@
 module ApplicationHelper
 
   # Use this to highlight active page's (toolbar or link).
-  # names - list of names for controllers with whom links will assosiated.
-  # current_controller - name of current controller.
-  def active_link?( names, current_controller )
-    names.each{ |name| return "active" if name == current_controller }
+  # :controllers - array of controllers. They can include their methods, for example: "pages", "pages#home".
+  # => "active" or "".
+  def active_link?( options={} )
+    unless options.empty?
+      controller_names = options[:controllers]
+
+      controller_names.each do |name|
+        if name.include?("#")                                                                       # The separator for method of controller. It may not exist.
+          splitten_data           = name.split("#")
+          current_controller_name = splitten_data.first
+          current_action_name     = splitten_data.last
+
+          return "active" if ( current_controller_name == controller.controller_name ) and
+                             ( current_action_name == controller.action_name )
+        else
+          return "active" if name == controller.controller_name
+        end
+      end
+    end
 
     return ""
   end
+
 end
