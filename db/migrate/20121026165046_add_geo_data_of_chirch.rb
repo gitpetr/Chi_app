cb404277a2d7e6e82a600e785458bdbf9c35e09b
L Serj <gelvior@gmail.com>
2013-06-14 17:58:46 +0400

    Fix for coordinates.

--- db/migrate/20121026165046_add_geo_data_of_chirch.rb
+++ db/migrate/20121026165046_add_geo_data_of_chirch.rb
@@ -5,8 +5,8 @@ class AddGeoDataOfChirch < ActiveRecord::Migration
     Contact.create( :chirch_name => "Преображение во Христе",
                     :address => "г. Москва, метро Савеловская, ул. Сущевский вал, д.3/5",
                     :phone => "+7 (499) 973-10-81",
-                    :latitude => 55.793423,
-                    :longitude => 37.590503 )
+                    :latitude => 55.792898,
+                    :longitude => 37.590102 )
   end
 
   def down
