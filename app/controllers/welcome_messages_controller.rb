5394b49e43de6835fb245348e82841f6e42dca7b
L Serj <gelvior@gmail.com>
2013-06-15 20:22:23 +0400

    welcome_message is updatable now.

--- app/controllers/welcome_messages_controller.rb
+++ app/controllers/welcome_messages_controller.rb
@@ -1,7 +1,15 @@
 class WelcomeMessagesController < ApplicationController
+  load_and_authorize_resource
+
   def edit
   end
 
   def update
+    if @welcome_message.update_attributes( params[:welcome_message] )
+      redirect_to root_path
+      flash[:success] = "Saved!"
+    else
+      render 'edit'
+    end
   end
 end
