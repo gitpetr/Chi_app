3f7ad0e4fde9b197ffffe0d31f099e77a952bda4
L Serj <gelvior@gmail.com>
2013-06-15 21:58:02 +0400

    Test to check wysihtml5 existance.

--- spec/features/wysihtml5_editors_spec.rb
+++ spec/features/wysihtml5_editors_spec.rb
@@ -10,19 +10,41 @@ describe "Wysihtml5Editors" do
     fill_in "Ваш email",  :with => @admin.email
     fill_in "Пароль", :with => 'qwerty'
     click_button "Войти"
-
-    click_link "navbar-articles"                                                                    # Статьи. Clicking by id.
-    click_link "Новая статья"
   end
 
-  describe "Panel" do
+  describe "Panel in articles" do
+    before(:each) do
+      click_link "navbar-articles"                                                                  # Статьи. Clicking by id.
+      click_link "Новая статья"
+    end
+
     it "should have toolbar for default (russian) version", :js => true do
       page.should have_css('ul.wysihtml5-toolbar')
     end
 
     it "should have toolbar for english version", :js => true do
       page.find("#us_flag").click                                                                   # Click english flag by id.
-      sleep 2
+      sleep 3
+
+      page.should have_css('ul.wysihtml5-toolbar')
+    end
+  end
+
+  describe "Panel in welcome message" do
+    before(:each) do
+      FactoryGirl.create(:welcome_message)
+
+      click_link "navbar-home"                                                                      # Главная.
+      click_link "Редактировать"
+    end
+
+    it "should have toolbar for default (russian) version", :js => true do
+      page.should have_css('ul.wysihtml5-toolbar')
+    end
+
+    it "should have toolbar for english version", :js => true do
+      page.find("#us_flag").click
+      sleep 3
 
       page.should have_css('ul.wysihtml5-toolbar')
     end
