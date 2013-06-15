edf7622be57508861c261dac2ffd515c2aa1b740
L Serj <gelvior@gmail.com>
2013-06-15 20:03:47 +0400

    Migration to add some data for welcome_message.

---
+++ db/migrate/20130615160106_add_welcome_message.rb
@@ -0,0 +1,9 @@
+class AddWelcomeMessage < ActiveRecord::Migration
+  def up
+    WelcomeMessage.create( :content => "Добро пожаловать на наш сайт!" )
+  end
+
+  def down
+    WelcomeMessage.delete_all
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130615153319) do
+ActiveRecord::Schema.define(:version => 20130615160106) do
 
   create_table "albums", :force => true do |t|
     t.string   "name"
