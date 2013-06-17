69c1497c2470a67f7815da8593e8c13fa4415a10
L Serj <gelvior@gmail.com>
2013-06-17 16:26:08 +0400

    Tests for home index page.

--- spec/controllers/homes_controller_spec.rb
+++ spec/controllers/homes_controller_spec.rb
@@ -1,12 +1,36 @@
-# require 'spec_helper'
+require 'spec_helper'
 
-# describe HomesController do
+shared_examples "render-success" do
+  it "should access" do
+    get :index, :locale => :en
+    response.should be_success
+  end
+end
 
-#   describe "GET 'index'" do
-#     it "returns http success" do
-#       get 'index'
-#       response.should be_success
-#     end
-#   end
+describe HomesController do
+  render_views
 
-# end
+  before(:each) do
+    @admin = FactoryGirl.create( :admin )
+    @user  = FactoryGirl.create( :user )
+  end
+
+  describe "GET 'index'" do
+    describe "for non-signed users" do
+      include_examples "render-success"
+    end
+
+    describe "for signed-in users" do
+      before(:each){ test_sign_in( @user ) }
+
+      include_examples "render-success"
+    end
+
+    describe "for signed-in admin" do
+      before(:each){ test_sign_in( @admin ) }
+
+      include_examples "render-success"
+    end
+  end
+
+end
