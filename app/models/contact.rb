11e3f995db85095bb5889733a71e811049937e1f
L Serj <gelvior@gmail.com>
2013-07-09 19:51:09 +0400

    Unused method removed.

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -30,9 +30,4 @@ class Contact < ActiveRecord::Base
   validates :phone,
               :presence => { :message => :presense_message },
               :length   => { :maximum => 100, :message => :max_length_message }
-
-  def gmaps4rails_address
-    # Describe how to retrieve the address from your model, if you use directly a db column, you can dry your code, see wiki.
-    "#{self.street}, #{self.city}, #{self.country}"
-  end
 end
