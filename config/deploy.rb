ca701b1aef7b6b4b5874abcfaea4df145c06329b
L Serj <gelvior@gmail.com>
2013-07-10 22:52:13 +0400

    Comments deleted.

--- config/deploy.rb
+++ config/deploy.rb
@@ -26,11 +26,11 @@ namespace :deploy do
   end
 
   task :setup_config, roles: :app do
-    # sudo "ln -nfs #{current_path}/config/nginx.conf /etc/nginx/sites-enabled/#{application}"
-    # sudo "ln -nfs #{current_path}/config/unicorn_init.sh /etc/init.d/unicorn_#{application}"
+    sudo "ln -nfs #{current_path}/config/nginx.conf /etc/nginx/sites-enabled/#{application}"
+    sudo "ln -nfs #{current_path}/config/unicorn_init.sh /etc/init.d/unicorn_#{application}"
     run "mkdir -p #{shared_path}/config"
     run "mkdir -p #{shared_path}/config/initializers"
-    # put File.read("config/database.example.yml"), "#{shared_path}/config/database.yml"
+    put File.read("config/database.example.yml"), "#{shared_path}/config/database.yml"
     put File.read("config/initializers/secret_token.example.rb"), "#{shared_path}/config/initializers/secret_token.rb"
     puts "Now edit the config files in (and change secret key) #{shared_path}."
   end
@@ -53,29 +53,3 @@ namespace :deploy do
   end
   before "deploy", "deploy:check_revision"
 end
-
-# set :application, "set your application name here"
-# set :repository,  "set your repository location here"
-
-# # set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
-# # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
-
-# role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-# role :app, "your app-server here"                          # This may be the same as your `Web` server
-# role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-# role :db,  "your slave db-server here"
-
-# # if you want to clean up old releases on each deploy uncomment this:
-# # after "deploy:restart", "deploy:cleanup"
-
-# # if you're still using the script/reaper helper you will need
-# # these http://github.com/rails/irs_process_scripts
-
-# # If you are using Passenger mod_rails uncomment this:
-# # namespace :deploy do
-# #   task :start do ; end
-# #   task :stop do ; end
-# #   task :restart, :roles => :app, :except => { :no_release => true } do
-# #     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-# #   end
-# # end
