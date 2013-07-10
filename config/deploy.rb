041241c7e2a42ff2ea14f9c32d4064493ad079c9
L Serj <gelvior@gmail.com>
2013-07-10 22:28:42 +0400

    Edit.

--- config/deploy.rb
+++ config/deploy.rb
@@ -36,6 +36,7 @@ namespace :deploy do
   after "deploy:setup", "deploy:setup_config"
 
   task :symlink_config, roles: :app do
+    run "ln -nfs #{shared_path}/config/initializers/secret_token.rb #{release_path}/config/initializers/secret_token.rb"
     run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
     run "ln -nfs #{shared_path}/uploads #{release_path}/public/uploads"
   end
