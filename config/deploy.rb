2621ecc21e640d14a112fcd72a95e22d502eca27
L Serj <gelvior@gmail.com>
2013-07-10 22:30:31 +0400

    Edit.

--- config/deploy.rb
+++ config/deploy.rb
@@ -28,7 +28,7 @@ namespace :deploy do
   task :setup_config, roles: :app do
     # sudo "ln -nfs #{current_path}/config/nginx.conf /etc/nginx/sites-enabled/#{application}"
     # sudo "ln -nfs #{current_path}/config/unicorn_init.sh /etc/init.d/unicorn_#{application}"
-    # run "mkdir -p #{shared_path}/config"
+    run "mkdir -p #{shared_path}/config"
     # put File.read("config/database.example.yml"), "#{shared_path}/config/database.yml"
     put File.read("config/initializers/secret_token.example.rb"), "#{shared_path}/config/initializers/secret_token.rb"
     puts "Now edit the config files in (and change secret key) #{shared_path}."
