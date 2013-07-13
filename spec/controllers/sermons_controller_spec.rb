a7c0dd3a940d53fe861d16182a90c2d42bca1c06
L Serj <gelvior@gmail.com>
2013-07-13 16:40:44 +0400

    Tests for index page.

--- spec/controllers/sermons_controller_spec.rb
+++ spec/controllers/sermons_controller_spec.rb
@@ -1,5 +1,35 @@
 require 'spec_helper'
 
 describe SermonsController do
+	render_views
 
+	 before(:each) do
+    @admin = FactoryGirl.create( :admin )
+    @user  = FactoryGirl.create( :user )
+  end
+
+  describe "GET 'index'" do
+    shared_examples "sermons-render-success" do
+      it "should access" do
+        get :index, :locale => :en
+        response.should be_success
+      end
+    end
+
+    describe "for non-signed users" do
+      include_examples "sermons-render-success"
+    end
+
+    describe "for signed-in users" do
+      before(:each){ test_sign_in( @user ) }
+
+      include_examples "sermons-render-success"
+    end
+
+    describe "for signed-in admin" do
+      before(:each){ test_sign_in( @admin ) }
+
+      include_examples "sermons-render-success"
+    end
+  end
 end
