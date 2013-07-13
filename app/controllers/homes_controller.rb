c352398b833288f43d836f0ac3313c0b7d3e84bf
L Serj <gelvior@gmail.com>
2013-07-13 16:22:47 +0400

    Autorize home page.

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -2,6 +2,8 @@ class HomesController < ApplicationController
   layout "home", :only => [ :index ]
 
   def index
+  	authorize! :index, :home
+
     @photos = Photo.order(:created_at).last(2).reverse
     @welcome_message = WelcomeMessage.first
   end
