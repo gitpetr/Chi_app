e4d39dd62b4b5d9fcd41cdce5bd7bc1f7cc03d56
L Serj <gelvior@gmail.com>
2013-07-13 16:06:15 +0400

    Validations for sermon.

--- app/models/sermon.rb
+++ app/models/sermon.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+#
+# Table name: sermons
+#
+#  id            :integer          not null, primary key
+#  title         :string(255)      default(""), not null
+#  recorded_date :datetime
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#
+
 class Sermon < ActiveRecord::Base
   attr_accessible :recorded_date, :title
+
+  validates :title,
+              :presence => { :message => :presense_message },
+              :length   => { :maximum => 250, :message => :max_length_message }
 end
--- spec/models/sermon_spec.rb
+++ spec/models/sermon_spec.rb
@@ -1,5 +1,36 @@
+# == Schema Information
+#
+# Table name: sermons
+#
+#  id            :integer          not null, primary key
+#  title         :string(255)      default(""), not null
+#  recorded_date :datetime
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#
+
 require 'spec_helper'
 
 describe Sermon do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @attr = { :title => "qwerty@qwerty.com", :recorded_date => DateTime.now }
+  end
+
+  describe "DB" do
+    it "should create sermon with valid params" do
+      expect do
+        Sermon.create( @attr )
+      end.to change( Sermon, :count ).by( 1 )
+    end
+  end
+
+  describe "Validations for" do
+    it "title can't be empty" do
+      Sermon.new( @attr.merge :title => "" ).should_not be_valid
+    end
+
+    it "title can't have more than 250 symbols" do
+      Sermon.new( @attr.merge( :title => "a" * 251 ) ).should_not be_valid
+    end
+  end
 end
