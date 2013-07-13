c3407e7bd0d96a4dea2021de242a2caa6d1099b7
L Serj <gelvior@gmail.com>
2013-07-13 17:21:38 +0400

    Addind a sermon (without a file for now).

--- app/controllers/sermons_controller.rb
+++ app/controllers/sermons_controller.rb
@@ -3,4 +3,16 @@ class SermonsController < ApplicationController
 
 	def index
 	end
+
+	def new
+	end
+
+	def create
+		if @sermon.save
+      redirect_to sermons_path
+      flash[:success] = t( :sermon_created_message )
+    else
+      render 'new'
+    end
+	end
 end
