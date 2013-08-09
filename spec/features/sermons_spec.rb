c78499055f5c1ff2d8ad3fffec1d0fe49b635d75
L Serj <gelvior@gmail.com>
2013-08-09 20:18:52 +0400

    Test for prev bugfix.

--- spec/features/sermons_spec.rb
+++ spec/features/sermons_spec.rb
@@ -1,33 +1,45 @@
 require 'spec_helper'
 
 describe "Sermons" do
-  before(:each) do
-    @user = FactoryGirl.create(:user)
-
-    visit root_path( :locale => "ru" )
-  end
-
-  describe "No sermons" do
-    it "shows index page if we don't have sermons" do
-      click_link "navbar-sermons"
-      current_path.should == sermons_path( :locale => "ru" )
-    end
-  end
-
-  describe "Uploaded" do
+  describe "Admin" do
     before(:each) do
-      @sermon = FactoryGirl.create(:sermon)
+      @admin = FactoryGirl.create(:admin)
 
-      click_link "navbar-sermons"
+      visit new_user_session_path( :locale => "ru" )
+      fill_in "Ваш email",  :with => @admin.email
+      fill_in "Пароль", :with => 'qwerty'
+      click_button "Войти"
     end
 
-    it "shows show page if we have at least 1 sermon" do
-      current_path.should == sermon_path( :locale => "ru", :id => @sermon )
+    describe "No sermons" do
+      before(:each) do
+        click_link "navbar-sermons"
+      end
+
+      it "shows index page if we don't have sermons" do
+        current_path.should == sermons_path( :locale => "ru" )
+      end
+
+      it "should have button to upload a sermon" do
+        page.should have_css('#sermon-add')
+      end
     end
 
-    it "shows audio player if we have at least 1 sermon", :js => true do
-      sleep 2
-      page.should have_css('div.jp-type-single')
+    describe "Uploaded" do
+      before(:each) do
+        @sermon = FactoryGirl.create(:sermon)
+
+        click_link "navbar-sermons"
+      end
+
+      it "shows show page if we have at least 1 sermon" do
+        current_path.should == sermon_path( :locale => "ru", :id => @sermon )
+      end
+
+      it "shows audio player if we have at least 1 sermon", :js => true do
+        sleep 2
+        page.should have_css('div.jp-type-single')
+      end
     end
   end
 end
