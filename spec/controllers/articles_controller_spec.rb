0d9743d108a3f1b5fc6d8a256d52d82a820fe02c
Serj L <gelvior@gmail.com>
2014-01-03 21:20:44 +0400

    Article's specs for controller: put, destroy, create specs + new hash syntax + `let` instead of before each.

--- spec/controllers/articles_controller_spec.rb
+++ spec/controllers/articles_controller_spec.rb
@@ -1,17 +1,13 @@
 require 'spec_helper'
 
 describe ArticlesController do
-  render_views
-
-  before(:each) do
-    @admin = FactoryGirl.create( :admin )
-    @user  = FactoryGirl.create( :user )
-  end
+  let(:admin) { FactoryGirl.create( :admin ) }
+  let(:user)  { FactoryGirl.create( :user ) }
 
   describe "GET 'index'" do
     shared_examples "article-render-success" do
       it "should access" do
-        get :index, :locale => :en
+        get :index, locale: :en
         response.should be_success
       end
     end
@@ -21,24 +17,24 @@ describe ArticlesController do
     end
 
     describe "for signed-in users" do
-      before(:each){ test_sign_in( @user ) }
+      before(:each){ test_sign_in( user ) }
 
       include_examples "article-render-success"
     end
 
     describe "for signed-in admin" do
-      before(:each){ test_sign_in( @admin ) }
+      before(:each){ test_sign_in( admin ) }
 
       include_examples "article-render-success"
     end
   end
 
   describe "GET 'show'" do
-    before(:each){ @article = FactoryGirl.create :article }
+    let(:article) { FactoryGirl.create :article }
 
     shared_examples "article-render-success-and-show" do
       it "should access" do
-        get :show, :id => @article, :locale => :en
+        get :show, id: article, locale: :en
         response.should be_success
       end
     end
@@ -48,13 +44,13 @@ describe ArticlesController do
     end
 
     describe "for signed-in users" do
-      before(:each){ test_sign_in( @user ) }
+      before(:each){ test_sign_in( user ) }
 
       include_examples "article-render-success-and-show"
     end
 
     describe "for signed-in admin" do
-      before(:each){ test_sign_in( @admin ) }
+      before(:each){ test_sign_in( admin ) }
 
       include_examples "article-render-success-and-show"
     end
@@ -63,7 +59,7 @@ describe ArticlesController do
   describe "GET 'new'" do
     shared_examples "article-new-render-success" do
       it "should deny access" do
-        get :new, :locale => :en
+        get :new, locale: :en
         response.should redirect_to( root_path )
       end
     end
@@ -73,35 +69,33 @@ describe ArticlesController do
     end
 
     describe "for signed-in users" do
-      before(:each){ test_sign_in( @user ) }
+      before(:each){ test_sign_in( user ) }
 
       include_examples "article-new-render-success"
     end
 
     describe "for signed-in admin" do
-      before(:each){ test_sign_in( @admin ) }
+      before(:each){ test_sign_in( admin ) }
 
       it "should access" do
-        get :new, :locale => :en
+        get :new, locale: :en
         response.should be_success
       end
     end
   end
 
   describe "POST 'create'" do
-    before(:each) do
-      @article = FactoryGirl.build(:article, :user => @admin, :content => "some", :title => "bla-bla")
-    end
+    let(:attrs) { {content: "some", title: "text"} }
 
     shared_examples "article-redirect-to-root-and-not-create" do
       it "should deny access" do
-        post :create, :locale => :en, :article => @article
+        post :create, locale: :en, article: attrs
         response.should redirect_to( root_path )
       end
 
       it "should not create article" do
         expect do
-          post :create, :locale => :en, :article => @article
+          post :create, locale: :en, article: attrs
         end.to_not change( Article, :count )
       end
     end
@@ -111,31 +105,28 @@ describe ArticlesController do
     end
 
     describe "for signed-in users" do
-      before(:each){ test_sign_in( @user ) }
+      before(:each){ test_sign_in( user ) }
 
       include_examples "article-redirect-to-root-and-not-create"
     end
 
-    # describe "for signed-in admin" do
-    #   it "should create article" do
-    #     sign_in :article, @admin
+    describe "for signed-in admin" do
+      it "should create article" do
+        test_sign_in( admin )
 
-    #     expect do
-    #       post :create, :locale => :en, :article => @article#.attributes.merge(:content => "some" )
-    #       # post :create, :locale => :en, :articles => { :content => "some", :title => "text" }, :user_id => @admin.id
-    #     end.should change( Article, :count ).by(1)
-    #   end
-    # end
+        expect do
+          post :create, locale: :en, article: attrs
+        end.to change( Article, :count ).by(1)
+      end
+    end
   end
 
   describe "GET 'edit'" do
-    before(:each) do
-      @article = FactoryGirl.create(:article, :user => @admin, :content => "some", :title => "bla-bla")
-    end
+    let(:article) { FactoryGirl.create(:article, user: admin) }
 
     shared_examples "article-edit-redirect-to-root" do
       it "should deny access" do
-        get :edit, :locale => :en, :id => @article
+        get :edit, locale: :en, id: article
         response.should redirect_to( root_path )
       end
     end
@@ -145,35 +136,87 @@ describe ArticlesController do
     end
 
     describe "for signed-in users" do
-      before(:each){ test_sign_in( @user ) }
+      before(:each){ test_sign_in( user ) }
 
       include_examples "article-edit-redirect-to-root"
     end
 
     describe "for signed-in admin" do
-      before(:each){ test_sign_in( @admin ) }
+      before(:each){ test_sign_in( admin ) }
 
       it "should access" do
-        get :edit, :locale => :en, :id => @article
+        get :edit, locale: :en, id: article
         response.should be_success
       end
     end
   end
 
-  # describe "PUT 'update'" do
-  #   before(:each) do
-  #     @article = FactoryGirl.create(:article, :user => @admin, :content => "some", :title => "bla-bla")
-  #   end
+  describe "PUT 'update'" do
+    let(:article) { FactoryGirl.create(:article, user: admin) }
+    let(:attrs) { {content: "some", title: "last"} }
 
-  #   describe "for signed-in admin" do
-  #     before(:each){ test_sign_in( @admin ) }
+    describe "for non-signed users" do
+      it "should deny access" do
+        put :update, locale: :en, id: article, article: attrs
+        response.should redirect_to( root_path )
+      end
+    end
 
-  #     it "should be done" do
-  #       text = "last"
-  #       put :update, :locale => :en, :id => @article, :article => @article.attributes.merge( :title => text )
-  #       @article.reload
-  #       @article.title.should == text
-  #     end
-  #   end
-  # end
+    describe "for signed-in users" do
+      before(:each){ test_sign_in( user ) }
+
+      it "should not update" do
+        put :update, locale: :en, id: article, article: attrs
+        article.reload
+        article.title.should_not == attrs[:title]
+      end
+    end
+
+    describe "for signed-in admin" do
+      before(:each){ test_sign_in( admin ) }
+
+      it "should update" do
+        put :update, locale: :en, id: article, article: attrs
+        article.reload
+        article.title.should == attrs[:title]
+      end
+    end
+  end
+
+  describe "DELETE 'destroy'" do
+    let!(:article) { FactoryGirl.create(:article, user: admin) }
+
+    shared_examples "article-redirect-to-root-and-not-delete" do
+      it "should deny access" do
+        delete :destroy, locale: :en, id: article
+        response.should redirect_to( root_path )
+      end
+
+      it "should not delete" do
+        expect do
+          delete :destroy, locale: :en, id: article
+        end.to_not change( Article, :count )
+      end
+    end
+
+    describe "for non-signed users" do
+      include_examples 'article-redirect-to-root-and-not-delete'
+    end
+
+    describe "for signed-in users" do
+      before(:each){ test_sign_in( user ) }
+
+      include_examples 'article-redirect-to-root-and-not-delete'
+    end
+
+    describe "for signed-in admins" do
+      before(:each){ test_sign_in( admin ) }
+
+      it "should delete" do
+        expect do
+          delete :destroy, locale: :en, id: article
+        end.to change(Article, :count).by(-1)
+      end
+    end
+  end
 end
