2c25208e6f761899f315cc9b52aa527b65cc0caa
Serj L <gelvior@gmail.com>
2014-01-07 19:48:30 +0400

    HomesController's specs updated: new hash syntax, let instead of before each.

--- spec/controllers/homes_controller_spec.rb
+++ spec/controllers/homes_controller_spec.rb
@@ -1,17 +1,13 @@
 require 'spec_helper'
 
 describe HomesController do
-  render_views
-
-  before(:each) do
-    @admin = FactoryGirl.create( :admin )
-    @user  = FactoryGirl.create( :user )
-  end
+  let(:admin) { FactoryGirl.create( :admin ) }
+  let(:user)  { FactoryGirl.create( :user ) }
 
   describe "GET 'index'" do
     shared_examples "home-render-success" do
       it "should access" do
-        get :index, :locale => :en
+        get :index, locale: :en
         response.should be_success
       end
     end
@@ -21,16 +17,15 @@ describe HomesController do
     end
 
     describe "for signed-in users" do
-      before(:each){ test_sign_in( @user ) }
+      before(:each){ test_sign_in( user ) }
 
       include_examples "home-render-success"
     end
 
     describe "for signed-in admin" do
-      before(:each){ test_sign_in( @admin ) }
+      before(:each){ test_sign_in( admin ) }
 
       include_examples "home-render-success"
     end
   end
-
 end
