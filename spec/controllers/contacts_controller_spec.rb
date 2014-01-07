a6054c62f0ca3ad36081ab7214c5e7f066894822
Serj L <gelvior@gmail.com>
2014-01-07 19:44:55 +0400

    Contacts spec refactored: new hash syntax, duplication removed via shared_examples, test for updating, let instead of before each.

--- spec/controllers/contacts_controller_spec.rb
+++ spec/controllers/contacts_controller_spec.rb
@@ -1,65 +1,96 @@
 require 'spec_helper'
 
 describe ContactsController do
-  render_views
-
-  before(:each) do
-    @admin = FactoryGirl.create( :admin )
-    @user  = FactoryGirl.create( :user )
-  end
+  let(:admin) { FactoryGirl.create( :admin ) }
+  let(:user) { FactoryGirl.create( :user ) }
 
   describe "GET 'index'" do
-    describe "for non-signed users" do
+    shared_examples "contact-render-success" do
       it "should access" do
-        get :index, :locale => :en
+        get :index, locale: :en
         response.should be_success
       end
+    end
+
+    describe "for non-signed users" do
+      include_examples "contact-render-success"
     end
 
     describe "for signed-in users" do
-      before(:each){ test_sign_in( @user ) }
+      before(:each){ test_sign_in( user ) }
 
-      it "should access" do
-        get :index, :locale => :en
-        response.should be_success
-      end
+      include_examples "contact-render-success"
     end
 
     describe "for signed-in admin" do
-      before(:each){ test_sign_in( @admin ) }
+      before(:each){ test_sign_in( admin ) }
 
-      it "should access" do
-        get :index, :locale => :en
-        response.should be_success
-      end
+      include_examples "contact-render-success"
     end
   end
 
   describe "GET 'edit'" do
-    before(:each){ @contact = FactoryGirl.create(:contact) }
+    let(:contact) { FactoryGirl.create(:contact) }
 
-    describe "for non-signed users" do
+    shared_examples "contact-edit-render-success" do
       it "should access" do
-        get :edit, :locale => :en, :id => @contact
-        response.should_not be_success
+        get :index, locale: :en
+        response.should be_success
       end
     end
 
+    describe "for non-signed users" do
+      include_examples "contact-edit-render-success"
+    end
+
     describe "for signed-in users" do
-      before(:each){ test_sign_in( @user ) }
+      before(:each){ test_sign_in( user ) }
 
       it "should not access" do
-        get :edit, :locale => :en, :id => @contact
+        get :edit, locale: :en, id: contact
         response.should_not be_success
       end
     end
 
     describe "for signed-in admin" do
-      before(:each){ test_sign_in( @admin ) }
+      before(:each){ test_sign_in( admin ) }
 
-      it "should access" do
-        get :edit, :locale => :en, :id => @contact
-        response.should be_success
+      include_examples "contact-edit-render-success"
+    end
+  end
+
+  describe "PUT 'update'" do
+    let(:contact) { FactoryGirl.create(:contact) }
+    let(:attrs) { { chirch_name: "Here is the name",
+                    phone: "+7 (499) 973-10-81",
+                    address: "Here is the address",
+                    latitude: "55",
+                    longitude: "37" } }
+
+    describe "for non-signed users" do
+      it "should deny access" do
+        put :update, locale: :en, id: contact, contact: attrs
+        response.should redirect_to( root_path )
+      end
+    end
+
+    describe "for signed-in users" do
+      before(:each){ test_sign_in( user ) }
+
+      it "should not update" do
+        put :update, locale: :en, id: contact, contact: attrs
+        contact.reload
+        contact.chirch_name.should_not == attrs[:chirch_name]
+      end
+    end
+
+    describe "for signed-in admin" do
+      before(:each){ test_sign_in( admin ) }
+
+      it "should update" do
+        put :update, locale: :en, id: contact, contact: attrs
+        contact.reload
+        contact.chirch_name.should == attrs[:chirch_name]
       end
     end
   end
